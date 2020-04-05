d18f993589ac04e1eeecd3ece333d9d8d8f777a9
Ewaen Isoken <EwaenIsoken@gmail.com>
2020-04-05 20:46:57 +0100

    Update TotalOrderDao.java
    Completed Module

--- src/main/java/com/pluralsight/order/dao/TotalOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/TotalOrderDao.java
@@ -33,7 +33,7 @@ public class TotalOrderDao {
         try (Connection con = database.getConnection();
              CallableStatement cs = createCallableStatement(con, paramsDto.getCustomerId())
         ) {
-            cs.executeUpdate();
+            cs.execute();
             ResultSet resultSet = cs.getResultSet();
 
             if(resultSet != null && resultSet.next() == true)
